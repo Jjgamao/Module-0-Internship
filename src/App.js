7fb1b30c93e0e436f9787468baf20cc0cc4f0781
Jjgamao <95895486+Jjgamao@users.noreply.github.com>
2022-03-26 14:11:33 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,299 @@
+
+import React from "react";
+import './styles.css';
+
+function App() {
+  return <Internship {...internshipData} />;
+}
+
+export default App;
+
+function Internship(props) {
+  const {
+    image24,
+    title,
+    navigationBar,
+    place,
+    module0IsMytSoft,
+    applyNow,
+    responsibilities,
+    image3,
+    learningTheCodebas,
+    image4,
+    writingAndMaintainingCode,
+    internPerks,
+    mentorship,
+    internshipAllowance,
+    accessToOurProjectsAndEvents,
+    companyLunchAndMeetups,
+    graduates,
+    image23,
+    name,
+    theInternshipWasF,
+    image25,
+    image26,
+    highlights,
+    contributionsOfThe,
+    image27,
+    overlapGroup17,
+    image29,
+    image30,
+    module0Internship,
+    mytSoftdevSolutions,
+    istockphoto1148911628612X6121,
+    gotMoreQuestions,
+    addressCebuPhili,
+    lgoRemovebgPreview3,
+    contactUs,
+  } = props;
+
+  return (
+    <div className="container-center-horizontal">
+      <div className="internshipscreen">
+        <div className="flex-col">
+          <div className="overlap-group4">
+            <img className="image-24" src={image24} />
+            <h1 className="titlevalign-text-middle">
+              {title}
+            </h1>
+            <div className="rectangle-6"></div>
+            <div className="rectangle-7"></div>
+            <img className="navigation-bar" src={navigationBar} />
+          </div>
+          <div className="placevalign-text-middle">
+            {place}
+          </div>
+          <div className="rectangle-9"></div>
+          <div className="overlap-group12roboto-normal-serenade-20px">
+            <div className="module-0-is-myt-softvalign-text-middle">
+              {module0IsMytSoft}
+            </div>
+            <div className="rectangle-20"></div>
+            <div className="apply-nowvalign-text-middle">
+              {applyNow}
+            </div>
+          </div>
+          <div className="overlap-group11">
+            <div className="rectangle-21"></div>
+            <div className="responsibilitiesvalign-text-middleroboto-bold-cello-30px">
+              {responsibilities}
+            </div>
+          </div>
+          <div className="group-container">
+            <img className="group-7" src="group-7.svg" />
+            <div className="overlap-group13">
+              <img className="image-3" src={image3} />
+              <div className="learning-the-codebasvalign-text-middleroboto-bold-serenade-20px">
+                {learningTheCodebas}
+              </div>
+            </div>
+          </div>
+          <div className="overlap-group-container">
+            <div className="overlap-group9">
+              <img className="group-8" src="group-8.svg" />
+              <div className="rectangle-34"></div>
+              <img className="image-4" src={image4} />
+              <div className="writing-and-maintaining-codevalign-text-middleroboto-bold-serenade-20px">
+                {writingAndMaintainingCode}
+              </div>
+            </div>
+            <div className="overlap-group10">
+              <div className="rectangle-35"></div>
+              <img className="group-10" src="group-10.svg" />
+            </div>
+          </div>
+          <div className="group-container-1">
+            <img className="group-12" src="group-12.svg" />
+            <img className="group-11" src="group-11.svg" />
+          </div>
+          <div className="group-container-2">
+            <img className="group-13" src="group-13.svg" />
+            <img className="group-14" src="group-14.svg" />
+          </div>
+          <div className="group-container-3">
+            <img className="group-18" src="group-18.svg" />
+            <img className="group-17" src="group-17.svg" />
+          </div>
+          <div className="rectangle-50"></div>
+          <div className="intern-perksinter-bold-cello-30px">
+            {internPerks}
+          </div>
+          <div className="overlap-group-container-1">
+            <div className="overlap-group19">
+              <div className="group-22"></div>
+              <div className="mentorshipvalign-text-middleroboto-bold-serenade-20px">
+                {mentorship}
+              </div>
+            </div>
+            <div className="overlap-group8">
+              <img className="mask-group" src="mask-group.svg" />
+              <div className="internship-allowancevalign-text-middleroboto-bold-serenade-20px">
+                {internshipAllowance}
+              </div>
+            </div>
+            <div className="overlap-group6">
+              <img className="mask-group-1" src="mask-group-2.svg" />
+              <div className="access-to-our-projects-and-eventsvalign-text-middleroboto-bold-serenade-20px">
+                {accessToOurProjectsAndEvents}
+              </div>
+            </div>
+            <div className="overlap-group14">
+              <img className="mask-group-2" src="mask-group-1.svg" />
+              <div className="company-lunch-and-meetupsvalign-text-middleroboto-bold-serenade-20px">
+                {companyLunchAndMeetups}
+              </div>
+            </div>
+          </div>
+          <div className="overlap-group7">
+            <div className="rectangle-51"></div>
+            <img className="group-6" src="group-6.svg" />
+            <img className="arrow-right-circle" src="arrow-right-circle.svg" />
+            <div className="rectangle-58"></div>
+            <div className="graduatesvalign-text-middleroboto-bold-cello-30px">
+              {graduates}
+            </div>
+            <div className="rectangle-57"></div>
+            <img className="group-4" src="group-4.svg" />
+            <div className="rectangle-59"></div>
+            <img className="image-23" src={image23} />
+            <div className="rectangle-60"></div>
+            <div className="namevalign-text-middle">
+              {name}
+            </div>
+            <p className="the-internship-was-fvalign-text-middle">
+              {theInternshipWasF}
+            </p>
+            <img className="group-4-1" src="group-4-1.svg" />
+          </div>
+        </div>
+        <div className="flex-row">
+          <div className="rectangle-68"></div>
+          <div className="overlap-group-container-2">
+            <div className="overlap-group20">
+              <div className="rectangle-62"></div>
+              <img className="image-25" src={image25} />
+            </div>
+            <div className="overlap-group18">
+              <div className="rectangle-66"></div>
+              <img className="image-26" src={image26} />
+            </div>
+          </div>
+          <div className="flex-col-1">
+            <div className="flex-row-1">
+              <div className="overlap-group-container-3">
+                <div className="overlap-group16">
+                  <div className="highlightsvalign-text-middleinter-bold-cello-30px">
+                    {highlights}
+                  </div>
+                  <p className="contributions-of-thevalign-text-middle">
+                    {contributionsOfThe}
+                  </p>
+                </div>
+                <div className="overlap-group3">
+                  <img className="image-27" src={image27} />
+                </div>
+              </div>
+              <div className="overlap-group17" style={{ backgroundImage: `url(${overlapGroup17})` }}></div>
+            </div>
+            <div className="overlap-group5">
+              <div className="rectangle-64"></div>
+              <img className="image-29" src={image29} />
+            </div>
+            <div className="overlap-group15">
+              <div className="rectangle-67"></div>
+              <img className="image-30" src={image30} />
+            </div>
+          </div>
+        </div>
+        <div className="group-26">
+          <div className="overlap-group">
+            <div className="overlap-group2">
+              <div className="overlap-group1">
+                <div className="rectangle-17"></div>
+                <div className="module-0-internshipvalign-text-middle">
+                  {module0Internship}
+                </div>
+                <div className="myt-softdev-solutionsvalign-text-middle">
+                  {mytSoftdevSolutions}
+                </div>
+                <img className="istockphoto-1148911628-612x612-1" src={istockphoto1148911628612X6121} />
+              </div>
+              <p className="got-more-questionsvalign-text-middle">
+                {gotMoreQuestions}
+              </p>
+              <p className="address-cebu-philivalign-text-middle">
+                {addressCebuPhili}
+              </p>
+            </div>
+            <div className="flex-col-2">
+              <img className="lgo-removebg-preview-3" src={lgoRemovebgPreview3} />
+              <div className="contact-usvalign-text-middle">
+                {contactUs}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const internshipData = {
+  image24: "",
+  title: "INTERNSHIP",
+  navigationBar: "",
+  place: "HOME",
+  module0IsMytSoft:
+    "Module 0 is MYT Softdev Solutions' internship program that equips interns with the right skills that will set you for the industry. Yes, you heard that right! We will be teaching you technical and soft skills you will need in creating innovative, intuitive and powerful solutions.",
+  applyNow: "APPLY NOW",
+  responsibilities: "RESPONSIBILITIES",
+  image3: "image-3.png",
+  learningTheCodebas: "Learning the codebase and improving your coding skills.",
+  image4: "image-4.png",
+  writingAndMaintainingCode: "Writing and maintaining code.",
+  internPerks: "INTERN PERKS",
+  mentorship: "Mentorship",
+  internshipAllowance: "Internship Allowance",
+  accessToOurProjectsAndEvents: (
+    <>
+      Access to our <br />
+      projects and events
+    </>
+  ),
+  companyLunchAndMeetups: (
+    <>
+      Company lunch <br />
+      and meetups
+    </>
+  ),
+  graduates: "GRADUATES",
+  image23: "image-23.png",
+  name: "Justin Chu",
+  theInternshipWasF:
+    "The Internship was fullfiling. I learned a lot and improve my skills in web development. It was more than I expected.",
+  image25: "image-25.png",
+  image26: "image-26.png",
+  highlights: "HIGHLIGHTS",
+  contributionsOfThe: "Contributions of the Interns to the Company",
+  image27: "image-27.png",
+  overlapGroup17: "image-28.png",
+  image29: "image-29.png",
+  image30: "image-30.png",
+  module0Internship: "MODULE 0 INTERNSHIP",
+  mytSoftdevSolutions: "MYT SOFTDEV SOLUTIONS",
+  istockphoto1148911628612X6121: "istockphoto-1148911628-612x612-1.png",
+  gotMoreQuestions: "Got more questions? Feel free to email us with your queries and concerns",
+  addressCebuPhili: (
+    <>
+      Address: Cebu, Philippines
+      <br />
+      <br />
+      Contact Number: +63 917 327 6230
+      <br />
+      <br />
+      Email: info@mytsoftdevsolutions.com
+    </>
+  ),
+  lgoRemovebgPreview3: "lgo-removebg-preview-3.png",
+  contactUs: "CONTACT US",
+};
